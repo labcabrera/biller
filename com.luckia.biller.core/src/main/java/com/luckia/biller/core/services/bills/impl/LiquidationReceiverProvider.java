3d9b157c9d07ff204f6e400435d30920518a4302
lab.cabrera@gmail.com <lab.cabrera@gmail.com>
2014-05-21 20:44:06 +0200

    PATCH

--- com.luckia.biller.core/src/main/java/com/luckia/biller/core/services/bills/impl/LiquidationReceiverProvider.java
+++ com.luckia.biller.core/src/main/java/com/luckia/biller/core/services/bills/impl/LiquidationReceiverProvider.java
@@ -22,7 +22,7 @@ public class LiquidationReceiverProvider {
 	public LegalEntity getReceiver() {
 		EntityManager entityManager = entityManagerProvider.get();
 		TypedQuery<Company> queryEgasa = entityManager.createQuery("select e from Company e where e.name = :name", Company.class);
-		List<Company> list = queryEgasa.setParameter("name", "'EGASA HATTRICK S.A.'").getResultList();
+		List<Company> list = queryEgasa.setParameter("name", "EGASA HATTRICK S.A.").getResultList();
 		return list.iterator().next();
 	}
 }
