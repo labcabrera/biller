be8a079148a4b40e65b7a661259652cdb1c8d90f
lab.cabrera@gmail.com <lab.cabrera@gmail.com>
2014-05-22 18:48:18 +0200

    Patch facturas locales

---
+++ com.luckia.biller.deploy/src/main/java/com/luckia/biller/deploy/Patch20140522A.java
@@ -0,0 +1,35 @@
+package com.luckia.biller.deploy;
+
+import java.util.Date;
+
+import org.apache.commons.lang3.Range;
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.luckia.biller.core.LuckiaCoreModule;
+import com.luckia.biller.core.jpa.EntityManagerProvider;
+import com.luckia.biller.core.scheduler.tasks.BillTask;
+import com.luckia.biller.core.services.bills.BillProcessor;
+
+public class Patch20140522A {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+	public static void main(String[] args) {
+		LOG.info("Ejecutando patch");
+		long[] storeIds = { 10162, 10172, 10166, 10168, 10164, 10167, 10169 };
+		Date from = new DateTime(2014, 4, 1, 0, 0, 0, 0).toDate();
+		Date to = new DateTime(2014, 4, 30, 0, 0, 0, 0).toDate();
+		Injector injector = Guice.createInjector(new LuckiaCoreModule());
+		Range<Date> range = Range.between(from, to);
+		EntityManagerProvider entityManagerProvider = injector.getInstance(EntityManagerProvider.class);
+		BillProcessor billProcessor = injector.getInstance(BillProcessor.class);
+		for (long storeId : storeIds) {
+			BillTask task = new BillTask(storeId, range, entityManagerProvider, billProcessor);
+			task.run();
+		}
+	}
+}
