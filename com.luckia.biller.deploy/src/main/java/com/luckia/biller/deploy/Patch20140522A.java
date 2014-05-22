d28c7ec8c0bbd3b429733797d62d8515ab09b340
lab.cabrera@gmail.com <lab.cabrera@gmail.com>
2014-05-22 19:19:22 +0200

    Patch

--- com.luckia.biller.deploy/src/main/java/com/luckia/biller/deploy/Patch20140522A.java
+++ com.luckia.biller.deploy/src/main/java/com/luckia/biller/deploy/Patch20140522A.java
@@ -12,24 +12,38 @@ import com.google.inject.Injector;
 import com.luckia.biller.core.LuckiaCoreModule;
 import com.luckia.biller.core.jpa.EntityManagerProvider;
 import com.luckia.biller.core.scheduler.tasks.BillTask;
+import com.luckia.biller.core.scheduler.tasks.LiquidationTask;
 import com.luckia.biller.core.services.bills.BillProcessor;
+import com.luckia.biller.core.services.bills.LiquidationProcessor;
 
 public class Patch20140522A {
 
 	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+	private static final boolean PROCESS_STORES = false;
+	private static final boolean PROCESS_COMPANIES = true;
 
 	public static void main(String[] args) {
 		LOG.info("Ejecutando patch");
-		long[] storeIds = { 2205, 2207, 2209, 2211, 2213, 2215, 2217 };
 		Date from = new DateTime(2014, 4, 1, 0, 0, 0, 0).toDate();
 		Date to = new DateTime(2014, 4, 30, 0, 0, 0, 0).toDate();
 		Injector injector = Guice.createInjector(new LuckiaCoreModule());
 		Range<Date> range = Range.between(from, to);
 		EntityManagerProvider entityManagerProvider = injector.getInstance(EntityManagerProvider.class);
 		BillProcessor billProcessor = injector.getInstance(BillProcessor.class);
-		for (long storeId : storeIds) {
-			BillTask task = new BillTask(storeId, range, entityManagerProvider, billProcessor);
-			task.run();
+		LiquidationProcessor liquidationProcessor = injector.getInstance(LiquidationProcessor.class);
+		if (PROCESS_STORES) {
+			long[] storeIds = { 2205, 2207, 2209, 2211, 2213, 2215, 2217 };
+			for (long storeId : storeIds) {
+				BillTask task = new BillTask(storeId, range, entityManagerProvider, billProcessor);
+				task.run();
+			}
+		}
+		if (PROCESS_COMPANIES) {
+			long[] companyIds = { 210, 219 };
+			for (long companyId : companyIds) {
+				LiquidationTask task = new LiquidationTask(companyId, range, entityManagerProvider, liquidationProcessor);
+				task.run();
+			}
 		}
 	}
 }
