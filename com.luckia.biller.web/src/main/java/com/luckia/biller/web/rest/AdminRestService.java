2927f4d43acaf041d501a7079e04ab2e1033346b
lab.cabrera@gmail.com <lab.cabrera@gmail.com>
2014-05-23 12:37:34 +0200

    MINOR: deshabilitamos recalcular facturas hasta solucionar error REST

--- com.luckia.biller.web/src/main/java/com/luckia/biller/web/rest/AdminRestService.java
+++ com.luckia.biller.web/src/main/java/com/luckia/biller/web/rest/AdminRestService.java
@@ -33,6 +33,7 @@ public class AdminRestService {
 
 	private static final Logger LOG = LoggerFactory.getLogger(AdminRestService.class);
 	private static final int RECALCULATE_BILLS_THREAD_COUNT = 10;
+	private static final boolean DISABLE_RECALCULATE = true;
 
 	@Inject
 	private EntityManagerProvider entityManagerProvider;
@@ -45,46 +46,50 @@ public class AdminRestService {
 	@Path("/recalculate/bills/{year}/{month}")
 	@ClearCache
 	public Message<String> find(@PathParam("year") Integer year, @PathParam("month") Integer month) {
-		LOG.info("Recalculando facturacion de {}/{}", year, month);
-		DateTime from = new DateTime(year, month, 1, 0, 0, 0, 0);
-		DateTime to = from.dayOfMonth().withMaximumValue();
-		EntityManager entityManager = entityManagerProvider.get();
-		String qlString = "select b.id from Bill b where b.billDate >= :from and b.billDate <= :to and b.currentState.stateDefinition.id in :states";
-		TypedQuery<String> query = entityManager.createQuery(qlString, String.class);
-		query.setParameter("from", from.toDate());
-		query.setParameter("to", to.toDate());
-		query.setParameter("states", Arrays.asList(CommonState.Initial, CommonState.Draft, CommonState.Empty));
-		List<String> billIds = query.getResultList();
+		if (DISABLE_RECALCULATE) {
+			return new Message<String>(Message.CODE_GENERIC_ERROR, "Opción deshabilitada temporalmente");
+		} else {
+			LOG.info("Recalculando facturacion de {}/{}", year, month);
+			DateTime from = new DateTime(year, month, 1, 0, 0, 0, 0);
+			DateTime to = from.dayOfMonth().withMaximumValue();
+			EntityManager entityManager = entityManagerProvider.get();
+			String qlString = "select b.id from Bill b where b.billDate >= :from and b.billDate <= :to and b.currentState.stateDefinition.id in :states";
+			TypedQuery<String> query = entityManager.createQuery(qlString, String.class);
+			query.setParameter("from", from.toDate());
+			query.setParameter("to", to.toDate());
+			query.setParameter("states", Arrays.asList(CommonState.Initial, CommonState.Draft, CommonState.Empty));
+			List<String> billIds = query.getResultList();
 
-		ExecutorService executorService = Executors.newFixedThreadPool(RECALCULATE_BILLS_THREAD_COUNT);
-		Long t0 = System.currentTimeMillis();
-		for (String billId : billIds) {
-			BillRecalculationTask task = new BillRecalculationTask(billId, entityManagerProvider, billProcessor);
-			executorService.submit(task);
-		}
-		LOG.debug("Esperando a la finalizacion de {} tareas (hilos: {})", billIds.size(), RECALCULATE_BILLS_THREAD_COUNT);
-		executorService.shutdown();
-		try {
-			executorService.awaitTermination(5, TimeUnit.HOURS);
-			LOG.debug("Finalizadas {} tareas en {} ms", billIds.size(), (System.currentTimeMillis() - t0));
-		} catch (InterruptedException ex) {
-			LOG.error("Error durante la ejecucion de las tareas", ex);
-		}
-
-		// Paso 2 buscar las nuevas empresas que hayan sido creadas que carecen de facturas
-		LOG.info("Buscando nuevos establecimientos para los que hay que generar las facturas");
-		TypedQuery<Store> storeQuery = entityManager.createQuery("select s from Store s order by s.name", Store.class);
-		TypedQuery<Bill> queryBills = entityManager.createQuery("select b from Bill b where b.sender = :store and b.billDate >= :from and b.billDate <= :to", Bill.class);
-		List<Store> stores = storeQuery.getResultList();
-		for (Store store : stores) {
-			queryBills.setParameter("store", store);
-			queryBills.setParameter("from", from.toDate());
-			queryBills.setParameter("to", to.toDate());
-			if (queryBills.getResultList().isEmpty()) {
-				LOG.debug("Detectado establecimiento sin facturas: " + store.getName());
-				// TODO
+			ExecutorService executorService = Executors.newFixedThreadPool(RECALCULATE_BILLS_THREAD_COUNT);
+			Long t0 = System.currentTimeMillis();
+			for (String billId : billIds) {
+				BillRecalculationTask task = new BillRecalculationTask(billId, entityManagerProvider, billProcessor);
+				executorService.submit(task);
 			}
+			LOG.debug("Esperando a la finalizacion de {} tareas (hilos: {})", billIds.size(), RECALCULATE_BILLS_THREAD_COUNT);
+			executorService.shutdown();
+			try {
+				executorService.awaitTermination(5, TimeUnit.HOURS);
+				LOG.debug("Finalizadas {} tareas en {} ms", billIds.size(), (System.currentTimeMillis() - t0));
+			} catch (InterruptedException ex) {
+				LOG.error("Error durante la ejecucion de las tareas", ex);
+			}
+
+			// Paso 2 buscar las nuevas empresas que hayan sido creadas que carecen de facturas
+			LOG.info("Buscando nuevos establecimientos para los que hay que generar las facturas");
+			TypedQuery<Store> storeQuery = entityManager.createQuery("select s from Store s order by s.name", Store.class);
+			TypedQuery<Bill> queryBills = entityManager.createQuery("select b from Bill b where b.sender = :store and b.billDate >= :from and b.billDate <= :to", Bill.class);
+			List<Store> stores = storeQuery.getResultList();
+			for (Store store : stores) {
+				queryBills.setParameter("store", store);
+				queryBills.setParameter("from", from.toDate());
+				queryBills.setParameter("to", to.toDate());
+				if (queryBills.getResultList().isEmpty()) {
+					LOG.debug("Detectado establecimiento sin facturas: " + store.getName());
+					// TODO
+				}
+			}
+			return new Message<String>(Message.CODE_SUCCESS, String.format("Recalculadas %s facturas", billIds.size()));
 		}
-		return new Message<String>(Message.CODE_SUCCESS, String.format("Recalculadas %s facturas", billIds.size()));
 	}
 }
