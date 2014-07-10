bc5a7841f45f7bd1a5677497756a6df81bdb84f4
Luis Barragan Cabrera <lab.cabrera@gmail.com>
2014-07-10 11:44:00 +0200

    FIX: insert store

--- com.luckia.biller.core/src/main/java/com/luckia/biller/core/validation/StoreValidator.java
+++ com.luckia.biller.core/src/main/java/com/luckia/biller/core/validation/StoreValidator.java
@@ -3,10 +3,15 @@ package com.luckia.biller.core.validation;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.luckia.biller.core.model.Store;
 import com.luckia.biller.core.model.validation.ValidStore;
 
 public class StoreValidator implements ConstraintValidator<ValidStore, Store> {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(StoreValidator.class);
 
 	@Override
 	public void initialize(ValidStore constraintAnnotation) {
@@ -16,10 +21,12 @@ public class StoreValidator implements ConstraintValidator<ValidStore, Store> {
 	public boolean isValid(Store entity, ConstraintValidatorContext context) {
 		boolean hasErrors = false;
 		if (entity.getBillingModel() == null) {
+			LOG.warn("Invalid store: model is null");
 			context.buildConstraintViolationWithTemplate("store.missing.model").addConstraintViolation();
 			hasErrors = true;
 		}
 		if (entity.getOwner() == null) {
+			LOG.warn("Invalid store: owner is null");
 			context.buildConstraintViolationWithTemplate("store.missing.owner").addConstraintViolation();
 			hasErrors = true;
 		}
