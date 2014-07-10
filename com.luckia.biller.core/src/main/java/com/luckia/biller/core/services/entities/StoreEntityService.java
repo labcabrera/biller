142db72a191f7dea4b10075ac6d62bca373e8778
Luis Barragan Cabrera <lab.cabrera@gmail.com>
2014-07-10 11:51:40 +0200

    FIX: insert store

--- com.luckia.biller.core/src/main/java/com/luckia/biller/core/services/entities/StoreEntityService.java
+++ com.luckia.biller.core/src/main/java/com/luckia/biller/core/services/entities/StoreEntityService.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import javax.persistence.EntityManager;
 
+import org.apache.commons.lang3.Validate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,18 +13,20 @@ import com.luckia.biller.core.model.Store;
 import com.luckia.biller.core.model.common.Message;
 
 public class StoreEntityService extends LegalEntityBaseService<Store> {
-	
+
 	private static final Logger LOG = LoggerFactory.getLogger(StoreEntityService.class);
 
 	@Override
 	@ClearCache
 	public Message<Store> merge(Store entity) {
+		Validate.notNull(entity);
+		LOG.info("Merge store {}", entity.getName());
 		Message<Store> validationResult = validate(entity);
 		if (validationResult.hasErrors()) {
 			return validationResult;
 		}
 		EntityManager entityManager = entityManagerProvider.get();
-		if(entityManager.getTransaction().isActive()) {
+		if (entityManager.getTransaction().isActive()) {
 			try {
 				LOG.warn("Transaccion en curso no esperada: forzando commit");
 				entityManager.getTransaction().commit();
@@ -35,12 +38,14 @@ public class StoreEntityService extends LegalEntityBaseService<Store> {
 		Store current;
 		String message;
 		if (entity.getId() == null) {
+			LOG.info("Creating new store");
 			current = new Store();
 			current.merge(entity);
 			auditService.processCreated(current);
 			entityManager.persist(current);
 			message = i18nService.getMessage("store.persist");
 		} else {
+			LOG.info("Creating new store");
 			current = entityManager.find(Store.class, entity.getId());
 			current.merge(entity);
 			auditService.processModified(current);
