d8cc988cd8ae1bdfb6ecbd5281168cb7c7429367
Luis Barragan Cabrera <lab.cabrera@gmail.com>
2014-09-04 12:45:42 +0200

    MINOR: cambios en los rangos de la generacion de facturas en entorno de desarrollo

--- com.luckia.biller.core/src/test/java/com/luckia/biller/core/scheduler/TestBillerJob.java
+++ com.luckia.biller.core/src/test/java/com/luckia/biller/core/scheduler/TestBillerJob.java
@@ -1,7 +1,5 @@
 package com.luckia.biller.core.scheduler;
 
-import java.util.Date;
-
 import org.apache.commons.lang3.Range;
 import org.joda.time.DateTime;
 import org.junit.Test;
@@ -18,9 +16,14 @@ public class TestBillerJob {
 		Injector injector = Guice.createInjector(new LuckiaCoreModule());
 		BillingJob job = injector.getInstance(BillingJob.class);
 		job.injector = injector;
-		Date from = new DateTime(2014, 1, 1, 0, 0, 0, 0).toDate();
-		Date to = new DateTime(2014, 1, 31, 0, 0, 0, 0).toDate();
-		job.execute(Range.between(from, to), 10);
-	}
 
+		int monthFrom = 1;
+		int monthTo = 7;
+
+		for (int month = monthFrom; month <= monthTo; month++) {
+			DateTime from = new DateTime(2014, month, 1, 0, 0, 0, 0);
+			DateTime to = from.dayOfMonth().withMaximumValue();
+			job.execute(Range.between(from.toDate(), to.toDate()), 10);
+		}
+	}
 }
