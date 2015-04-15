426e3702ec296d8e682b0d7194336524a787a330
lab.cabrera@gmail.com <lab.cabrera@gmail.com>
2015-04-15 22:06:11 +0200

    Cambios report liquidaciones

--- biller-web/src/main/webapp/js/reporting-ctrl.js
+++ biller-web/src/main/webapp/js/reporting-ctrl.js
@@ -11,7 +11,10 @@
 
 	billerControllers.controller('ReportLiquidationsCtrl', [ '$scope', '$location', '$window', '$filter', function($scope, $location, $window, $filter) {
 		$scope.generate = function() {
-			var ids = '201';
+			var ids = '';
+			if($scope.reportData != null && $scope.reportData.company != null) {
+				ids = $scope.reportData.company.id;
+			}
 			var from = $filter('date')($scope.reportData.from, "yyyy-MM-dd");
 			var to = $filter('date')($scope.reportData.to, "yyyy-MM-dd");
 			var url = REST_PATH + '/report/liquidations?ids=' + ids + '&from=' + from + '&to=' + to;
