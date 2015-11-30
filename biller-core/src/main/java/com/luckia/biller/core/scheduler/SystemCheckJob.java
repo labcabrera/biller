ab156f01a23ef88dac8c2eca6ebf67d83a677071
lab.cabrera@gmail.com <lab.cabrera@gmail.com>
2015-11-30 10:51:20 +0100

    Cambios menores

--- biller-core/src/main/java/com/luckia/biller/core/scheduler/SystemCheckJob.java
+++ biller-core/src/main/java/com/luckia/biller/core/scheduler/SystemCheckJob.java
@@ -2,8 +2,6 @@ package com.luckia.biller.core.scheduler;
 
 import java.io.File;
 
-import javax.inject.Inject;
-
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
@@ -17,25 +15,29 @@ public class SystemCheckJob extends BaseJob {
 
 	private static final Logger LOG = LoggerFactory.getLogger(SystemCheckJob.class);
 
-	@Inject
 	private AlertService alertService;
 
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
 		LOG.trace("Checking system status");
 		init(context);
+		alertService = injector.getInstance(AlertService.class);
 		checkRepositoryFolder();
 	}
 
 	private void checkRepositoryFolder() {
 		String repositoryPath = injector.getInstance(Key.get(String.class, Names.named("repositoryPath")));
-		File folder = new File(repositoryPath);
-		if (!folder.canRead()) {
-			alertService.hangleAlert(String.format("Missing repository folder '%s'", repositoryPath));
-		} else if (folder.canRead()) {
-			alertService.hangleAlert(String.format("Invalid folder permissions '%s': cant read", repositoryPath));
-		} else if (folder.canWrite()) {
-			alertService.hangleAlert(String.format("Invalid folder permissions '%s': cant write", repositoryPath));
+		if (repositoryPath == null) {
+			alertService.hangleAlert("Missing configuration repository folder. Check app configuration");
+		} else {
+			File folder = new File(repositoryPath);
+			if (!folder.canRead()) {
+				alertService.hangleAlert(String.format("Missing repository folder '%s'", repositoryPath));
+			} else if (folder.canRead()) {
+				alertService.hangleAlert(String.format("Invalid folder permissions '%s': cant read", repositoryPath));
+			} else if (folder.canWrite()) {
+				alertService.hangleAlert(String.format("Invalid folder permissions '%s': cant write", repositoryPath));
+			}
 		}
 	}
 }
