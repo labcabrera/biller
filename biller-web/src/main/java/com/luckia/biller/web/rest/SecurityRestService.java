61e588e81218ee4959c7ae4a9d9500821523876f
lab.cabrera@gmail.com <lab.cabrera@gmail.com>
2015-11-30 10:06:54 +0100

    Trazas revisando error login

--- biller-web/src/main/java/com/luckia/biller/web/rest/SecurityRestService.java
+++ biller-web/src/main/java/com/luckia/biller/web/rest/SecurityRestService.java
@@ -5,9 +5,13 @@ import java.util.Map;
 import javax.annotation.security.PermitAll;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.gson.JsonElement;
 import com.luckia.biller.core.model.common.Message;
@@ -15,6 +19,8 @@ import com.luckia.biller.core.services.security.UserSessionService;
 
 @Path("/security")
 public class SecurityRestService {
+
+	private static final Logger LOG = LoggerFactory.getLogger(SecurityRestService.class);
 
 	@Inject
 	private UserSessionService userSessionService;
@@ -24,16 +30,28 @@ public class SecurityRestService {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@PermitAll
 	public Message<Map<String, Object>> login(JsonElement request) {
-		String key = request.getAsJsonObject().get("user").getAsString();
-		String password = request.getAsJsonObject().get("password").getAsString();
-		return userSessionService.login(key, password);
+		LOG.debug("Received login request");
+		try {
+			String key = request.getAsJsonObject().get("user").getAsString();
+			String password = request.getAsJsonObject().get("password").getAsString();
+			return userSessionService.login(key, password);
+		} catch (Exception ex) {
+			LOG.error("Login request error", ex);
+			throw new InternalServerErrorException("Login error", ex);
+		}
 	}
 
 	@POST
 	@Path("/logout")
 	@PermitAll
 	public Message<String> logout(String sessionId) {
-		userSessionService.logout(sessionId);
-		return new Message<String>().withMessage("Success");
+		LOG.debug("Received logout request");
+		try {
+			userSessionService.logout(sessionId);
+			return new Message<String>().withMessage("Success");
+		} catch (Exception ex) {
+			LOG.error("Logout request error", ex);
+			throw new InternalServerErrorException("Logout error", ex);
+		}
 	}
 }
