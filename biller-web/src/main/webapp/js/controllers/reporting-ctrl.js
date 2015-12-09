ba8ecaa605212741bf325385a867ba0102a8613c
lab.cabrera@gmail.com <lab.cabrera@gmail.com>
2015-12-09 18:36:43 +0100

    Cambios reporting

--- biller-web/src/main/webapp/js/controllers/reporting-ctrl.js
+++ biller-web/src/main/webapp/js/controllers/reporting-ctrl.js
@@ -29,11 +29,16 @@
 	billerModule.controller('ReportLiquidationsSummaryCtrl', [ '$scope', '$location', '$window', '$filter', function($scope, $location, $window, $filter) {
 		$scope.reportData = {};
 		$scope.generate = function() {
-			var from = $scope.reportData.from != null ? $filter('date')($scope.reportData.from, "yyyy-MM-dd") : '';
-			var to = $scope.reportData.to != null ? $filter('date')($scope.reportData.to, "yyyy-MM-dd") : '';
+			$scope.message = null;
+			var from = $scope.reportData.from != null ? $filter('date')($scope.reportData.from, "yyyy-MM-dd") : null;
+			var to = $scope.reportData.to != null ? $filter('date')($scope.reportData.to, "yyyy-MM-dd") : null;
 			var companyId = $scope.reportData.company != null ? $scope.reportData.company.id : "";
 			var costCenterId = $scope.reportData.costCenter != null ? $scope.reportData.costCenter.id : "";
 			var companyGroupId = $scope.reportData.companyGroup != null ? $scope.reportData.companyGroup.id : "";
+			if(from == null || to == null) {
+				$scope.message = {code: 200, warnings: [ 'Es necesario establecer las fechas' ]};
+				return;
+			}
 			var url = 'rest/report/liquidation-summary?from=' + from;
 			url += '&to=' + to;
 			url += "&companyId=" + companyId;
