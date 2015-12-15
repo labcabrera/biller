47552cbc5ce905b9ab548bf22f075c59d30f6c49
lab.cabrera@gmail.com <lab.cabrera@gmail.com>
2015-12-15 13:19:48 +0100

    MINOR: front number format

--- biller-core/src/main/java/com/luckia/biller/core/serialization/BigDecimalSerializer.java
+++ biller-core/src/main/java/com/luckia/biller/core/serialization/BigDecimalSerializer.java
@@ -4,12 +4,15 @@ import java.lang.reflect.Type;
 import java.math.BigDecimal;
 import java.text.NumberFormat;
 
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
-public class BigDecimalSerializer implements JsonSerializer<BigDecimal> {
+public class BigDecimalSerializer implements JsonSerializer<BigDecimal>, JsonDeserializer<BigDecimal> {
 
 	@Override
 	public JsonElement serialize(BigDecimal value, Type typeOfSrc, JsonSerializationContext context) {
@@ -18,10 +21,14 @@ public class BigDecimalSerializer implements JsonSerializer<BigDecimal> {
 			NumberFormat numberFormat = NumberFormat.getNumberInstance();
 			numberFormat.setMinimumFractionDigits(2);
 			numberFormat.setMaximumFractionDigits(2);
-			numberFormat.setGroupingUsed(false);
+			numberFormat.setGroupingUsed(true);
 			return new JsonPrimitive(numberFormat.format(value));
 		}
 		return result;
 	}
 
+	@Override
+	public BigDecimal deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+		return json != null ? new BigDecimal(json.getAsString().replaceAll(",", "")) : null;
+	}
 }
